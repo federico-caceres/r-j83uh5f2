1d65f6ecf5e7a3ca1c074ff3863dc0ddb4f78aa0
Federico Cáceres <federicoenmanuel@gmail.com>
2023-01-18 19:28:40 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      counter: 0
+    }
+  }
+
+  handlerOnChange = (e) => {
+    const value = e.target.value
+    this.setState({
+      counter: value.length
+    });
+  };
+
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.handlerOnChange}></textarea>
+        <div className="counter">{ this.state.counter }</div>
       </div>
     );
   }
